0b2830b2a7ed7b19acd3cb4217bf527228d84aa8
SuzukiYuki <yukicourage3111@gmail.com>
2021-03-07 22:22:03 +0900

    MVC構築

---
+++ app/Http/Controllers/PlaceController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Place;
+use Illuminate\Http\Request;
+
+class PlaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    // 一覧表示
+    public function index()
+    {
+        $places = Place::paginate();
+        return view ('places.index', compact('Places'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    // 追加画面表示
+    public function create()
+    {
+        return view ('Places.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+
+    // 詳細表示画面
+    public function show(Place $place)
+    {
+        return view ('Places.show',compact('Places'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+
+    // 修正画面
+    public function edit(Place $place)
+    {
+        return view ('Places.modify',compact('Places'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Place $place)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Place $place)
+    {
+        //
+    }
+}
---
+++ resources/views/places/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('contents')
+
+<div>
+		<!-- ここだけタイトルを入れましょう。 -->
+    <h1>場所一覧|場所追加|場所詳細|場所修正</h1>
+</div>
+
+@endsection
