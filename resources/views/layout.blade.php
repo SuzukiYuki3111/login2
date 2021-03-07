e8dfebd149608f4bb8fc4dd03c3fde1271d9b6d4
SuzukiYuki <yukicourage3111@gmail.com>
2021-03-07 17:39:55 +0900

    blade修正

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DancingMap</title>
+</head>
+<body>
+    <header>
+        {{-- ログインしてなければ表示される --}}
+        @guest
+            <a href="{{ route ('login') }}">ログイン</a>
+            <a href="{{ route ('register') }}">会員登録</a>
+        @endguest
+
+        @auth
+            名前 {{ Auth::user()->name }}
+
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit"> ログアウト </button>
+            </form>
+        @endauth
+    </header>
+
+    @yield('contents')
+
+    <footer></footer>
+</body>
+</html>
