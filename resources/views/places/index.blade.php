e73ac0d1eb3dc013adacf94e9e7ecec2ebb82772
SuzukiYuki <yukicourage3111@gmail.com>
2021-03-08 11:53:15 +0900

    ビュー一覧追加

--- resources/views/places/index.blade.php
+++ resources/views/places/index.blade.php
@@ -3,8 +3,34 @@
 @section('contents')
 
 <div>
-		<!-- ここだけタイトルを入れましょう。 -->
-    <h1>場所一覧|場所追加|場所詳細|場所修正</h1>
+    <h1>場所一覧</h1>
+    <div>
+        <a href="{{ route('places.create') }}">場所追加</a>
+    </div>
+    @if (count > 0)
+        <table>
+            <tr>
+                <th>写真</th>
+                <th>名称</th>
+                <th>登録者</th>
+                <th>操作</th>
+            </tr>
+
+            @foreach ($places as $place)
+            <tr>
+                <td><img src="{{ asset('storage/' .$place->img) }}" width="50" height="50"></td>
+                <td>{{ $place->name }}</td>
+                <td>{{ $place->user->name }}</td>
+                <td></td>
+            </tr>
+            @endforeach
+
+            {{ $places->links() }}
+        </table>
+
+        @else
+        <p>登録されていません。</p>
+    @endif
 </div>
 
 @endsection
